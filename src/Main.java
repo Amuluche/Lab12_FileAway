12764fb437629fb6314f519a7b0b95fd6a309114
Victor Amuluche <amulucvc@mail.uc.edu>
2024-11-11 23:39:34 -0500

    Init

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        FileInspector i = new FileInspector();
+        i.readfile();
+        i.readFile2();
+        i.stop();
+    }
+}
